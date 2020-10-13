4fa36b681aae528b8d0d004fde65e4f83f92bfde
Hoang Vo <g@hvvo.dev>
2020-10-13 01:43:57 -0400

    Add back Recently Played

--- src/components/Room/RoomQueue.tsx
+++ src/components/Room/RoomQueue.tsx
@@ -1,13 +1,17 @@
 import React, { useCallback, useMemo, useState } from "react";
+import { useCurrentUser } from "~/hooks/user";
+import { Modal, useModal } from "~/components/Modal";
+import { QueueManager, QueueViewer, useQueue } from "~/components/Queue";
+import {
+  TrackAdderPlaylist,
+  TrackAdderSearch,
+} from "~/components/Track/TrackAdder";
 import {
   QueueAction,
   Room,
   RoomState,
   useUpdateQueueMutation,
 } from "~/graphql/gql.gen";
-import { useCurrentUser } from "~/hooks/user";
-import { QueueManager, useQueue } from "../Queue";
-import { TrackAdderPlaylist, TrackAdderSearch } from "../Track/TrackAdder";
 
 const RoomQueue: React.FC<{ room: Room; roomState?: RoomState }> = ({
   room,
@@ -49,72 +53,99 @@ const RoomQueue: React.FC<{ room: Room; roomState?: RoomState }> = ({
     [updateQueue, room]
   );
 
+  const [activePlayed, openPlayed, closePlayed] = useModal();
+
   return (
-    <div className="h-full flex flex-col">
-      <div role="tablist" className="flex flex-none">
-        <button
-          role="tab"
-          className={`flex-1 mx-1 p-1 text-sm rounded-lg font-bold ${
-            tab === "queue"
-              ? "bg-foreground bg-opacity-25 text-white"
-              : "opacity-75"
-          }`}
-          aria-controls="tabpanel_queue"
-          onClick={() => setTab("queue")}
-          aria-selected={tab === "queue"}
+    <>
+      <div className="h-full flex flex-col">
+        <div role="tablist" className="flex flex-none">
+          <button
+            role="tab"
+            className={`flex-1 mx-1 p-1 text-sm rounded-lg font-bold ${
+              tab === "queue"
+                ? "bg-foreground bg-opacity-25 text-white"
+                : "opacity-75"
+            }`}
+            aria-controls="tabpanel_queue"
+            onClick={() => setTab("queue")}
+            aria-selected={tab === "queue"}
+          >
+            Queue
+          </button>
+          <button
+            role="tab"
+            className={`flex-1 mx-1 p-1 text-sm rounded-lg font-bold  ${
+              tab === "song"
+                ? "bg-foreground bg-opacity-25 text-white"
+                : "opacity-75"
+            }`}
+            aria-controls="tabpanel_song"
+            onClick={() => setTab("song")}
+            aria-selected={tab === "song"}
+          >
+            Search
+          </button>
+          <button
+            role="tab"
+            className={`flex-1 mx-1 p-1 text-sm rounded-lg font-bold  ${
+              tab === "playlist"
+                ? "bg-foreground bg-opacity-25 text-white"
+                : "opacity-75"
+            }`}
+            aria-controls="tabpanel_playlist"
+            onClick={() => setTab("playlist")}
+            aria-selected={tab === "playlist"}
+          >
+            Playlist
+          </button>
+        </div>
+        <div
+          aria-hidden={tab !== "queue"}
+          className={`${
+            tab !== "queue" ? "hidden" : "flex"
+          } relative flex-col h-full`}
         >
-          Queue
-        </button>
-        <button
-          role="tab"
-          className={`flex-1 mx-1 p-1 text-sm rounded-lg font-bold  ${
-            tab === "song"
-              ? "bg-foreground bg-opacity-25 text-white"
-              : "opacity-75"
-          }`}
-          aria-controls="tabpanel_song"
-          onClick={() => setTab("song")}
-          aria-selected={tab === "song"}
+          <QueueManager
+            permission={permission}
+            rules={{ maxSongs: roomState?.queueMax ?? 0 }}
+            queueId={`room:${room.id}`}
+          />
+          <button
+            className="text-xs p-1 leading-none button button-light bottom-2 right-2 absolute"
+            onClick={openPlayed}
+          >
+            Recently Played
+          </button>
+        </div>
+        <div
+          aria-hidden={tab !== "song"}
+          className={`${tab !== "song" ? "hidden" : "flex"} flex-col h-full`}
         >
-          Search
-        </button>
-        <button
-          role="tab"
-          className={`flex-1 mx-1 p-1 text-sm rounded-lg font-bold  ${
-            tab === "playlist"
-              ? "bg-foreground bg-opacity-25 text-white"
-              : "opacity-75"
-          }`}
-          aria-controls="tabpanel_playlist"
-          onClick={() => setTab("playlist")}
-          aria-selected={tab === "playlist"}
+          <TrackAdderSearch callback={onAddTracks} addedTracks={addedTracks} />
+        </div>
+        <div
+          aria-hidden={tab !== "playlist"}
+          className={`${
+            tab !== "playlist" ? "hidden" : "flex"
+          } flex-col h-full`}
         >
-          Playlist
-        </button>
+          <TrackAdderPlaylist
+            callback={onAddTracks}
+            addedTracks={addedTracks}
+          />
+        </div>
       </div>
-      <div
-        aria-hidden={tab !== "queue"}
-        className={`${tab !== "queue" ? "hidden" : "flex"} flex-col h-full`}
-      >
-        <QueueManager
-          permission={permission}
-          rules={{ maxSongs: roomState?.queueMax ?? 0 }}
-          queueId={`room:${room.id}`}
-        />
-      </div>
-      <div
-        aria-hidden={tab !== "song"}
-        className={`${tab !== "song" ? "hidden" : "flex"} flex-col h-full`}
-      >
-        <TrackAdderSearch callback={onAddTracks} addedTracks={addedTracks} />
-      </div>
-      <div
-        aria-hidden={tab !== "playlist"}
-        className={`${tab !== "playlist" ? "hidden" : "flex"} flex-col h-full`}
-      >
-        <TrackAdderPlaylist callback={onAddTracks} addedTracks={addedTracks} />
-      </div>
-    </div>
+      <Modal.Modal active={activePlayed} onOutsideClick={closePlayed}>
+        <Modal.Header>
+          <Modal.Title>Recently Played</Modal.Title>
+        </Modal.Header>
+        <Modal.Content noPadding>
+          <div className="h-128">
+            <QueueViewer queueId={`room:${room.id}:played`} reverse />
+          </div>
+        </Modal.Content>
+      </Modal.Modal>
+    </>
   );
 };
 
