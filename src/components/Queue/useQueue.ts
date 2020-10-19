2d002cdc8d62102b360e1734ca06216403d2c491
Hoang Vo <g@hvvo.dev>
2020-10-19 08:15:23 -0400

    Do not subscribe to queue if queue not found

--- src/components/Queue/useQueue.ts
+++ src/components/Queue/useQueue.ts
@@ -7,6 +7,9 @@ export default function useQueue(queueId: string) {
   const [res] = useQueueQuery({
     variables: { id: queueId as string },
   });
-  useOnQueueUpdatedSubscription({ variables: { id: queueId as string } });
+  useOnQueueUpdatedSubscription({
+    variables: { id: queueId as string },
+    pause: !res.data?.queue,
+  });
   return [res.data?.queue, res] as const;
 }
