41c8fc47e380775a2c3189e515be55216103c91c
Hoang Vo <dev+g@hvvo.dev>
2020-11-28 05:29:22 -0500

    Styling NowPlayingReaction

--- src/components/NowPlaying/NowPlayingReaction.tsx
+++ src/components/NowPlaying/NowPlayingReaction.tsx
@@ -1,130 +1,96 @@
-import React, { useRef, useEffect, useCallback } from "react";
+import React, { useCallback } from "react";
+import { animated } from "react-spring";
 import { useToasts } from "~/components/Toast/index";
 import { usePlayer } from "~/components/Player/index";
 import { useCurrentUser } from "~/hooks/user";
 import {
   useReactNowPlayingMutation,
   NowPlayingReactionType,
-  NowPlayingReactionPartsFragment,
   useNowPlayingReactionsQuery,
   useOnNowPlayingReactionsUpdatedSubscription,
 } from "~/graphql/gql.gen";
+import { useBoop } from "~/hooks/animation";
 
-const NowPlayingReaction: React.FC<{ id: string }> = ({ id }) => {
-  const {
-    state: { playerPlaying },
-  } = usePlayer();
-  const user = useCurrentUser();
-  const toasts = useToasts();
-  const btnCrying = useRef<HTMLButtonElement>(null);
-  const btnHeart = useRef<HTMLButtonElement>(null);
-  const btnFire = useRef<HTMLButtonElement>(null);
-  const btnTearJoy = useRef<HTMLButtonElement>(null);
-  const prevNowPlayingReaction = useRef<NowPlayingReactionPartsFragment>({
-    id,
-    mine: null,
-    cry: 0,
-    heart: 0,
-    fire: 0,
-    joy: 0,
-  }).current;
-  const [{ data }] = useNowPlayingReactionsQuery({
+const ReactionTypeToCode: Record<NowPlayingReactionType, string> = {
+  [NowPlayingReactionType.Fire]: "1f525",
+  [NowPlayingReactionType.Cry]: "1f62d",
+  [NowPlayingReactionType.Heart]: "1f497",
+  [NowPlayingReactionType.Joy]: "1f602",
+};
+
+const NowPlayingReactionEntry: React.FC<{
+  reaction: NowPlayingReactionType;
+  id: string;
+}> = ({ reaction, id }) => {
+  const [
+    { data: { nowPlayingReactions } = { nowPlayingReactions: undefined } },
+  ] = useNowPlayingReactionsQuery({
     variables: { id },
     requestPolicy: "cache-and-network",
   });
-  useOnNowPlayingReactionsUpdatedSubscription({ variables: { id } });
-  const nowPlayingReaction = data?.nowPlayingReactions;
+
+  const {
+    state: { playerPlaying },
+  } = usePlayer();
+
+  const canReact = playerPlaying && !nowPlayingReactions?.mine;
+
+  const user = useCurrentUser();
+  const toasts = useToasts();
 
   const [, reactNowPlaying] = useReactNowPlayingMutation();
 
-  const react = useCallback(
-    (reaction: NowPlayingReactionType) => {
-      if (user) reactNowPlaying({ id, reaction });
-      else toasts.message("Join to Add Your Reaction");
-    },
-    [toasts, id, reactNowPlaying, user]
-  );
+  const [style, trigger] = useBoop({
+    y: 0,
+    scale: 1.1,
+    rotation: 10,
+  });
 
-  const canReact = playerPlaying && !nowPlayingReaction?.mine;
-
-  const getClassname = (type: NowPlayingReactionType) =>
-    `btn flex-1 transform transition p-1 mr-1 ${
-      nowPlayingReaction?.mine === type
-        ? "bg-pink opacity-100"
-        : "bg-opacity-25"
-    }`;
-
-  useEffect(() => {
-    // Add animation to buttons
-    if (!nowPlayingReaction) return;
-    if (nowPlayingReaction.fire > prevNowPlayingReaction.fire) {
-      btnFire.current?.classList.add("scale-75");
-      setTimeout(() => btnFire.current?.classList.remove("scale-75"), 200);
-    }
-    if (nowPlayingReaction.cry > prevNowPlayingReaction.cry) {
-      btnCrying.current?.classList.add("scale-75");
-      setTimeout(() => btnCrying.current?.classList.remove("scale-75"), 200);
-    }
-    if (nowPlayingReaction.heart > prevNowPlayingReaction.heart) {
-      btnHeart.current?.classList.add("scale-75");
-      setTimeout(() => btnHeart.current?.classList.remove("scale-75"), 200);
-    }
-    if (nowPlayingReaction.joy > prevNowPlayingReaction.joy) {
-      btnTearJoy.current?.classList.add("scale-75");
-      setTimeout(() => btnTearJoy.current?.classList.remove("scale-75"), 200);
-    }
-    Object.assign(prevNowPlayingReaction, nowPlayingReaction);
-  }, [
-    prevNowPlayingReaction,
-    nowPlayingReaction,
-    btnCrying,
-    btnTearJoy,
-    btnHeart,
-    btnFire,
-  ]);
+  const react = useCallback(() => {
+    if (!canReact) return;
+    trigger();
+    if (user) reactNowPlaying({ id, reaction });
+    else toasts.message("Join to Add Your Reaction");
+  }, [canReact, toasts, id, reactNowPlaying, user, reaction, trigger]);
 
   return (
-    <div className="flex text-sm">
-      <button
-        ref={btnHeart}
-        onClick={() => react(NowPlayingReactionType.Heart)}
-        className={getClassname(NowPlayingReactionType.Heart)}
-        disabled={!canReact}
+    <button
+      onClick={react}
+      className={`btn btn-transparent flex-none p-1 mr-2 ${
+        nowPlayingReactions?.mine === reaction ? "opacity-100" : ""
+      }`}
+      disabled={!canReact}
+    >
+      <animated.span
+        onMouseEnter={trigger}
+        style={style}
+        className="flex items-center w-full justify-center"
       >
-        <span role="img" aria-label="Heart">
-          {nowPlayingReaction?.heart || 0} ❤️
+        <img
+          alt={reaction}
+          src={`https://twemoji.maxcdn.com/svg/${ReactionTypeToCode[reaction]}.svg`}
+          className="w-6 mr-1 h-6"
+        />
+        <span className="text-foreground-secondary text-lg font-bold">
+          {nowPlayingReactions?.[reaction] || 0}
         </span>
-      </button>
-      <button
-        ref={btnFire}
-        className={getClassname(NowPlayingReactionType.Fire)}
-        onClick={() => react(NowPlayingReactionType.Fire)}
-        disabled={!canReact}
-      >
-        <span role="img" aria-label="Fire">
-          {nowPlayingReaction?.fire || 0} 🔥
-        </span>
-      </button>
-      <button
-        ref={btnTearJoy}
-        className={getClassname(NowPlayingReactionType.Joy)}
-        onClick={() => react(NowPlayingReactionType.Joy)}
-        disabled={!canReact}
-      >
-        <span role="img" aria-label="Face with Tears of Joy">
-          {nowPlayingReaction?.joy || 0} 😂
-        </span>
-      </button>
-      <button
-        ref={btnCrying}
-        className={getClassname(NowPlayingReactionType.Cry)}
-        onClick={() => react(NowPlayingReactionType.Cry)}
-        disabled={!canReact}
-      >
-        <span role="img" aria-label="Fire">
-          {nowPlayingReaction?.cry || 0} 😢
-        </span>
-      </button>
+      </animated.span>
+    </button>
+  );
+};
+
+const NowPlayingReaction: React.FC<{ id: string }> = ({ id }) => {
+  useOnNowPlayingReactionsUpdatedSubscription({ variables: { id } });
+
+  return (
+    <div className="flex text-sm overflow-y-hidden overflow-x-auto">
+      <NowPlayingReactionEntry
+        id={id}
+        reaction={NowPlayingReactionType.Heart}
+      />
+      <NowPlayingReactionEntry id={id} reaction={NowPlayingReactionType.Fire} />
+      <NowPlayingReactionEntry id={id} reaction={NowPlayingReactionType.Joy} />
+      <NowPlayingReactionEntry id={id} reaction={NowPlayingReactionType.Cry} />
     </div>
   );
 };
