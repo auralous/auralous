47234f33cccce040bcc32f852654fed932fabd04
Hoang Vo <dev+g@hvvo.dev>
2020-12-06 03:15:02 -0500

    remove search from browse

--- src/pages/browse.tsx
+++ src/pages/browse.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { NextPage } from "next";
-import { useRouter } from "next/router";
 import { NextSeo } from "next-seo";
 import { Tabs, TabList, Tab, TabPanels, TabPanel } from "@reach/tabs";
 import { StoryItem } from "~/components/Story";
 import { useExploreStoriesQuery, useStoriesQuery } from "~/graphql/gql.gen";
 import { useCurrentUser } from "~/hooks/user";
-import { SvgSearch } from "~/assets/svg";
 import { useI18n } from "~/i18n/index";
 
 const RandomStorySection: React.FC = () => {
@@ -54,43 +52,6 @@ const MyStoriesSection: React.FC = () => {
   );
 };
 
-const SearchAndPlaySection: React.FC = () => {
-  const { t } = useI18n();
-  const router = useRouter();
-  return (
-    <div className="text-white p-4 mb-8 bg-gradient-to-t from-blue to-blue-tertiary">
-      <h3 className="text-xl font-bold">{t("browse.playlist.title")}</h3>
-      <p className="text-sm text-foreground-secondary mb-1">
-        {t("browse.playlist.description")}
-      </p>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          const s = event.currentTarget.searchQuery.value.trim();
-          s && router.push(`/new?search=${encodeURIComponent(s)}`);
-        }}
-        className="flex"
-      >
-        <input
-          name="searchQuery"
-          className="input w-full rounded-r-none"
-          placeholder={t("new.fromSearch.altText")}
-          aria-label={t("new.fromSearch.altText")}
-          type="url"
-          required
-        />
-        <button
-          type="submit"
-          className="btn rounded-l-none"
-          title={t("new.playlist.title")}
-        >
-          <SvgSearch />
-        </button>
-      </form>
-    </div>
-  );
-};
-
 const StorySection: React.FC = () => {
   const { t } = useI18n();
   return (
@@ -128,12 +89,7 @@ const BrowsePage: NextPage = () => {
   return (
     <>
       <NextSeo title={t("browse.title")} />
-      <div className="">
-        <div className="mb-2">
-          <SearchAndPlaySection />
-        </div>
-        <StorySection />
-      </div>
+      <StorySection />
     </>
   );
 };
