a8ebe2b5d3dfb3f4bfcbd7325f7c59977621aaec
Hoang Vo <dev+g@hvvo.dev>
2020-12-09 12:47:39 -0500

    Move StoryQueueAdder

--- src/components/Story/StoryQueue.tsx
+++ src/components/Story/StoryQueue.tsx
@@ -9,21 +9,21 @@ import {
 import { useCurrentUser } from "~/hooks/user";
 import { QueueAction, Story, useUpdateQueueMutation } from "~/graphql/gql.gen";
 import { useI18n } from "~/i18n/index";
-import { SvgClock } from "~/assets/svg";
+import { SvgPlus } from "~/assets/svg";
 import { useLogin } from "../Auth";
 import { toast } from "~/lib/toast";
+import { Modal, useModal } from "~/components/Modal";
 
 const AnimatedTabPanel = animated(TabPanel);
 const tabInactiveStyle = { opacity: 0, transform: "translate3d(0px,40px,0px)" };
 const tabActiveStyle = { opacity: 1, transform: "translate3d(0px,0px,0px)" };
 
-const StoryQueue: React.FC<{ story: Story }> = ({ story }) => {
+const StoryQueueAdder: React.FC<{
+  story: Story;
+  active: boolean;
+  close(): void;
+}> = ({ story, active, close }) => {
   const { t } = useI18n();
-
-  const user = useCurrentUser();
-  const isQueueable = Boolean(
-    user && (story.creatorId === user.id || story.queueable.includes(user.id))
-  );
 
   const [, updateQueue] = useUpdateQueueMutation();
   const [queue] = useQueue(story.id);
@@ -60,74 +60,112 @@ const StoryQueue: React.FC<{ story: Story }> = ({ story }) => {
   const tabPanelStyle2 = useSpring(
     2 === selectedIndex ? tabActiveStyle : tabInactiveStyle
   );
-  const tabPanelStyle3 = useSpring(
-    3 === selectedIndex ? tabActiveStyle : tabInactiveStyle
+
+  return (
+    <Modal.Modal active={active} onOutsideClick={close}>
+      <Modal.Content className="h-screen-layout">
+        <Tabs
+          onChange={setSelectedIndex}
+          index={selectedIndex}
+          className="h-full flex flex-col overflow-hidden"
+        >
+          <TabList className="flex flex-none py-1 gap-1">
+            <Tab className={getClassName(0)}>
+              {t("story.queue.search.title")}
+            </Tab>
+            <Tab className={getClassName(1)}>
+              {t("story.queue.playlist.title")}
+            </Tab>
+            <Tab className={`${getClassName(2)}`}>
+              {t("story.queue.played.title")}
+            </Tab>
+          </TabList>
+          <TabPanels className="flex-1 h-0">
+            <AnimatedTabPanel
+              style={tabPanelStyle0}
+              className="h-full"
+              as="div"
+            >
+              <TrackAdderSearch
+                callback={onAddTracks}
+                addedTracks={addedTracks}
+              />
+            </AnimatedTabPanel>
+            <AnimatedTabPanel
+              style={tabPanelStyle1}
+              className="h-full"
+              as="div"
+            >
+              <TrackAdderPlaylist
+                callback={onAddTracks}
+                addedTracks={addedTracks}
+                queryConfig={
+                  selectedIndex === 2 ? undefined : { enabled: false }
+                }
+              />
+            </AnimatedTabPanel>
+            <AnimatedTabPanel
+              style={tabPanelStyle2}
+              className="h-full"
+              as="div"
+            >
+              <QueueViewer
+                onAdd={onAddTracks}
+                queueId={`${story.id}:played`}
+                reverse
+                queryOpts={selectedIndex === 2 ? undefined : { pause: true }}
+              />
+            </AnimatedTabPanel>
+          </TabPanels>
+        </Tabs>
+      </Modal.Content>
+      <Modal.Footer>
+        <button onClick={close} className="btn w-full">
+          {t("story.queue.adderClose")}
+        </button>
+      </Modal.Footer>
+    </Modal.Modal>
+  );
+};
+
+const StoryQueue: React.FC<{ story: Story }> = ({ story }) => {
+  const { t } = useI18n();
+
+  const user = useCurrentUser();
+  const isQueueable = Boolean(
+    user && (story.creatorId === user.id || story.queueable.includes(user.id))
   );
 
   const [, showLogin] = useLogin();
 
-  const onTabChange = useCallback(
-    (index: number) => {
-      if (index === 1 || index === 2) {
-        // These two requires user to log in
-        if (!user) return showLogin();
-        // ...and be allowed to add to queue
-        if (!isQueueable)
-          return toast.open({
-            type: "info",
-            message: t("story.queue.notAllowed"),
-          });
-      }
-      setSelectedIndex(index);
-    },
-    [showLogin, user, t, isQueueable]
-  );
+  const [active, open, close] = useModal();
+
+  const onAddButtonClick = () => {
+    if (!user) return showLogin();
+    if (!isQueueable)
+      return toast.open({
+        type: "info",
+        message: t("story.queue.notAllowed"),
+      });
+    open();
+  };
 
   return (
-    <Tabs
-      onChange={onTabChange}
-      index={selectedIndex}
-      className="h-full flex flex-col overflow-hidden"
-    >
-      <TabList className="flex flex-none py-1 gap-1">
-        <Tab className={getClassName(0)}>{t("story.queue.queue.title")}</Tab>
-        <Tab className={getClassName(1)}>{t("story.queue.search.title")}</Tab>
-        <Tab className={getClassName(2)}>{t("story.queue.playlist.title")}</Tab>
-        <Tab
-          className={`${getClassName(3)} flex-grow-0`}
-          title={t("story.queue.played.title")}
-        >
-          <SvgClock width="16" height="16" />
-        </Tab>
-      </TabList>
-      <TabPanels className="flex-1 h-0">
-        <AnimatedTabPanel style={tabPanelStyle0} className="h-full" as="div">
-          <QueueManager
-            isQueueable={isQueueable}
-            queueId={story.id}
-            onEmptyAddClick={() => setSelectedIndex(1)}
-          />
-        </AnimatedTabPanel>
-        <AnimatedTabPanel style={tabPanelStyle1} className="h-full" as="div">
-          <TrackAdderSearch callback={onAddTracks} addedTracks={addedTracks} />
-        </AnimatedTabPanel>
-        <AnimatedTabPanel style={tabPanelStyle2} className="h-full" as="div">
-          <TrackAdderPlaylist
-            callback={onAddTracks}
-            addedTracks={addedTracks}
-            queryConfig={selectedIndex === 2 ? undefined : { enabled: false }}
-          />
-        </AnimatedTabPanel>
-        <AnimatedTabPanel style={tabPanelStyle3} className="h-full" as="div">
-          <QueueViewer
-            onAdd={isQueueable ? onAddTracks : undefined}
-            queueId={`${story.id}:played`}
-            reverse
-            queryOpts={selectedIndex === 3 ? undefined : { pause: true }}
-          />
-        </AnimatedTabPanel>
-      </TabPanels>
-    </Tabs>
+    <div className="h-full relative">
+      <button
+        title={t("story.queue.adderTitle")}
+        onClick={onAddButtonClick}
+        className="z-40 btn btn-primary rounded-full absolute bottom-2 right-2 sm:bottom-4 sm:right-4 w-12 h-12 md:w-16 md:h-16 p-1"
+      >
+        <SvgPlus />
+      </button>
+      <QueueManager
+        isQueueable={isQueueable}
+        queueId={story.id}
+        onEmptyAddClick={open}
+      />
+      <StoryQueueAdder story={story} active={active} close={close} />
+    </div>
   );
 };
 
