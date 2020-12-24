12435a78ff92945920e5966e9a2070e04a49de04
Hoang Vo <dev+g@hvvo.dev>
2020-12-24 11:18:03 -0500

    fix bg

--- src/components/Player/PlayerMinibar.tsx
+++ src/components/Player/PlayerMinibar.tsx
@@ -59,7 +59,10 @@ const PlayerMinibar: React.FC = () => {
       hidden={
         !playingStoryId || noPlayerMinibarRoutes.includes(router.pathname)
       }
-      className="fixed sm:pl-48 h-14 z-10 w-full bottom-10 sm:bottom-0"
+      className="fixed h-14 z-10 w-full bottom-10 sm:bottom-0 border-t-4 border-primary"
+      style={{
+        background: "linear-gradient(180deg, hsl(232,12%,13%), rgb(18 18 24))",
+      }}
     >
       <PlayerMinibarBar />
     </div>
