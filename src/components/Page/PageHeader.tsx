603a272ea25b51c1cd40dff16b982a5d9d635043
Hoang Vo <dev+g@hvvo.dev>
2021-02-15 18:01:53 +0700

    styling

--- src/components/Page/PageHeader.tsx
+++ src/components/Page/PageHeader.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const PageHeader: React.FC<{ name: string }> = ({ name }) => {
   return (
-    <div className="p-4 pt-6">
+    <div className="px-4 mb-2 pt-4 pb-2 sticky top-0 z-10 backdrop-blur bg-background bg-opacity-50">
       <Typography.Title size="4xl">{name}</Typography.Title>
     </div>
   );
