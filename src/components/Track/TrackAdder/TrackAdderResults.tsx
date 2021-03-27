2dfe4397221da5f16ea81db63af87fd6f7cc0cf3
Hoang Vo <dev+g@hvvo.dev>
2021-03-27 12:50:04 +0700

    add Add all button

--- src/components/Track/TrackAdder/TrackAdderResults.tsx
+++ src/components/Track/TrackAdder/TrackAdderResults.tsx
@@ -65,13 +65,28 @@ const TrackAdderResults: React.FC<{
   callback: TrackAdderCallbackFn;
   addedTracks: string[];
 }> = ({ callback, results, addedTracks }) => {
+  const { t } = useI18n();
+
+  const addAll = () =>
+    callback(results.filter((r) => !addedTracks.includes(r)));
+
   // TODO: a11y
   return (
-    <div
-      role="listbox"
-      className="overflow-hidden flex-1 min-h-0 flex flex-col"
-    >
-      <AutoSizer defaultHeight={1} defaultWidth={1}>
+    <div role="listbox" className="items-center flex-1 min-h-0 flex flex-col">
+      {results.length > 0 && (
+        <Button
+          size="sm"
+          shape="circle"
+          title={t("trackAdder.result.addAll")}
+          style={{ marginBottom: ".1rem" }}
+          onClick={addAll}
+        />
+      )}
+      <AutoSizer
+        style={{ flex: 1, minHeight: 0, width: "100%" }}
+        defaultHeight={1}
+        defaultWidth={1}
+      >
         {({ height, width }) => (
           <FixedSizeList
             height={height}
