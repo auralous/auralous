d93c5834dd88625c012e84b9965a0fadca1a8525
Hoang Vo <dev+g@hvvo.dev>
2021-10-21 17:52:13 +0700

    app: delay player loading

--- app/src/player-components/PlayerComponent.tsx
+++ app/src/player-components/PlayerComponent.tsx
@@ -1,18 +1,24 @@
-import { usePlaybackAuthentication } from "@/player";
+import { usePlaybackAuthentication, usePlaybackTrackId } from "@/player";
 import { PlatformName } from "@auralous/api";
 import type { FC } from "react";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import PlayerSpotify from "./PlayerSpotify";
 import PlayerYoutube from "./PlayerYoutube";
 
 const PlayerComponent: FC = ({ children }) => {
   const { playingPlatform } = usePlaybackAuthentication();
+  const [loaded, setLoaded] = useState(false);
+
+  const trackId = usePlaybackTrackId();
+  useEffect(() => {
+    if (trackId) setLoaded(true);
+  }, [trackId]);
 
   const DynamicPlayer = useMemo(() => {
-    if (!playingPlatform) return null;
+    if (!loaded || !playingPlatform) return null;
     if (playingPlatform === PlatformName.Youtube) return PlayerYoutube;
     return PlayerSpotify;
-  }, [playingPlatform]);
+  }, [playingPlatform, loaded]);
 
   return (
     <>
