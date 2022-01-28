6ca5cd3df50396f34e8bfbbe9dc1a61f3c3fc9d2
Hoang <dev+g@hvvo.dev>
2022-01-28 17:13:06 +0700

    Remove react-native-image-colors

--- app/src/player-components/PlayerView/useTrackColor.ts
+++ app/src/player-components/PlayerView/useTrackColor.ts
@@ -1,13 +1,8 @@
-import { useImageColor } from "@/styles/utils";
-import { useTrackQuery } from "@auralous/api";
+import { Colors } from "@/styles/colors";
+
+// FIXME: Temporarily remove this feature
 
 export const useTrackColor = (trackId?: string | null) => {
-  const [{ data, stale }] = useTrackQuery({
-    variables: { id: trackId || "" },
-    pause: !trackId,
-  });
-
-  const trackImage = !!trackId && !stale ? data?.track?.image : undefined;
-
-  return useImageColor(trackImage);
+  if (!trackId) return Colors.background;
+  return Colors.background;
 };
