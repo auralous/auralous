d0306d8595f4e83fda7550cac925adf143709ab4
Hoang <dev+g@hvvo.dev>
2022-02-19 20:13:46 +0700

    app: update map style url

--- app/src/utils/constants.ts
+++ app/src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const STORAGE_KEY_SETTINGS_LANGUAGE = "settings/language";
 
 export const MAPBOX_STYLE_URL =
-  "mapbox://styles/hoangvvo/ckrxme9og2bmg18muulftvx53";
+  "mapbox://styles/hoangvvo/ckztu8ofy000f14nxzqebpc8y";
 
 export const supportedLanguages = ["en", "vi"];
