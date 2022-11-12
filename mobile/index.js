870918280335b5a54743ae5911a1cbf20b868bf9
Hoang Vo <dev+g@hvvo.dev>
2022-11-13 00:34:56 +0700

    Bump graphql libs

--- mobile/index.js
+++ mobile/index.js
@@ -1,5 +1,6 @@
 import { App } from "@auralous/app";
 import { AppRegistry } from "react-native";
+import "react-native-url-polyfill/auto";
 import { name as appName } from "./app.json";
 
 AppRegistry.registerComponent(appName, () => App);
